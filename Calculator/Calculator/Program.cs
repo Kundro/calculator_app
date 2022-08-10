81433182567911abed926cf410c350d748f63458
Alexey Kundro <exeykundro@gmail.com>
2022-08-10 18:50:58 +0300

    feat: check a non-zero divisor

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -2,12 +2,12 @@
 
 namespace Calculator
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             // Declare variables and then initialize to zero.
-            int num1 = 0; int num2 = 0;
+            double num1 = 0; double num2 = 0;
 
             // Display title as the C# console calculator app.
             Console.WriteLine("Console Calculator in C#\r");
@@ -15,11 +15,11 @@ namespace Calculator
 
             // Ask the user to type the first number.
             Console.WriteLine("Type a number, and then press Enter");
-            num1 = Convert.ToInt32(Console.ReadLine());
+            num1 = Convert.ToDouble(Console.ReadLine());
 
             // Ask the user to type the second number.
             Console.WriteLine("Type another number, and then press Enter");
-            num2 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToDouble(Console.ReadLine());
 
             // Ask the user to choose an option.
             Console.WriteLine("Choose an option from the following list:");
@@ -42,6 +42,12 @@ namespace Calculator
                     Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
                     break;
                 case "d":
+                    // Ask the user to enter a non-zero divisor until they do so.
+                    while (num2 == 0)
+                    {
+                        Console.WriteLine("Enter a non-zero divisor: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                    }
                     Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
                     break;
             }
