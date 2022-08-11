fe01091a94073bc72d70a55c484907df5371105c
Alexey Kundro <exeykundro@gmail.com>
2022-08-11 14:08:58 +0300

    feat: add ShouldNotDoOperation test

--- Calculator/tests/Calculator.Tests/CalculatorTests.cs
+++ Calculator/tests/Calculator.Tests/CalculatorTests.cs
@@ -18,5 +18,40 @@ namespace Calculator.Tests
             double result = Calculator.DoOperation(3, 2, "m");
             Assert.Equal(6, result);
         }
+
+        [Fact]
+        public void ShouldDoDivision()
+        {
+            double result = Calculator.DoOperation(3, 2, "d");
+            Assert.Equal(1.5, result);
+        }
+
+        [Fact]
+        public void ShouldDoSubtraction()
+        {
+            double result = Calculator.DoOperation(3, 2, "s");
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ShouldNotDoDivision()
+        {
+            double result = Calculator.DoOperation(3, 0, "d");
+            Assert.Equal(double.NaN, result);
+        }
+
+        [Fact]
+        public void ShouldNotDoOperation1()
+        {
+            double result = Calculator.DoOperation(4, 2, "");
+            Assert.Equal(double.NaN, result);
+        }
+
+        [Fact]
+        public void ShouldNotDoOperation2()
+        {
+            double result = Calculator.DoOperation(8, 4, "da");
+            Assert.Equal(double.NaN, result);
+        }
     }
 }
